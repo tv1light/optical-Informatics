975d8964221cbe0ac9c2929a07664292e21d2a23
v.gutorov <v.gutorov@bft.ru>
2024-12-16 05:58:01 +0400

    Revert "lab2"
    This reverts commit c732d993d27f024f9dbb31c2bc8c30e1b8c8651c.

--- lab1/main.py
+++ lab1/main.py
@@ -1,215 +1,43 @@
 import numpy as np
 import matplotlib.pyplot as plt
-a1 = -5
-a2 = 5
-N = 1 << 10
+from scipy.special import jv
 
-#функция, согласно варианту
-light = lambda x: np.sin(4*np.pi*x)
-x = np.linspace(a1, a2, N, endpoint=False)
-f = light(x)
+def draw(title, arr_x, arr_y):
+    """
+    Функция для постройки графика
 
-M = 1 << 16
-b2 = (N * N) / (4 * a2 * M)
-b1 = -b2
-u = np.linspace(b1, b2, N, endpoint=False)
-hx = (a2 - a1) / N
+    :param title:
+    :param arr_x:
+    :param arr_y:
+    :return:
+    """
 
-# БПФ
-def bpf(f, N, M, hx):
-  k = (M - N) // 2
-  F = np.insert(f, 0, np.zeros(k))
-  F = np.append(F, np.zeros(k))
+    plt.plot(arr_x, arr_y, 'm')
+    plt.title(title)
+    plt.grid(True)
+    plt.show()
 
-  F = np.fft.fftshift(F)
-  F = np.fft.fft(F) * hx
-  F = np.fft.fftshift(F)
+if __name__ == '__main__':
+    a, b = 0, 20
+    p, q = 0, 5
+    m, n = 1000, 1000
+    alpha = 1
 
-  F = F[len(F) // 2 - N // 2: len(F) // 2 + N // 2]
-  return F
+    #Исходный сигнал
+    betta = 1 / 10
+    func = lambda x: np.exp(complex(0, 1) * x * betta)
+    # arr_x = np.arange(a, b)
+    # arr_y = func(arr_x)
+    # draw("График амплитуды входного сигнала", arr_x, np.abs(arr_y))
+    # draw("График фазы входного сигнала", arr_x, np.angle(arr_y))
 
-F = bpf(f, N, M, hx)
-
-# Аналитическое преобразование Фурье
-def analitic_Furie(f):
-    h_x = 2*a2 / N
-    h_u = 2*b2 / N
-    F = 0
-    arr_F = []
-    for i_u in range(N):
-        for i_x in range (N):
-           F += h_x * (f[i_x]) * np.exp(-2 * np.pi * 1j * x[i_x] * (-b2 + i_u * h_u))
-        arr_F.append(F)
-        F = 0
-    return arr_F
-
-F_analytical = analitic_Furie(f)
-
-#графики исходного сигнала, согласно варианту
-plt.figure(figsize=(20,10))
-
-plt.subplot(2, 2, 1)
-plt.plot(x, np.abs(f))
-plt.title('Амплитуда светового поля, согласно варианту')
-plt.grid()
-
-plt.subplot(2, 2, 2)
-plt.plot(x, np.angle(f))
-plt.title('Фаза светового поля, согласно варианту')
-plt.grid()
-
-plt.subplot(2, 2, 3)
-plt.plot(u, np.abs(F), label='БПФ', c = 'c', lw = 10)
-plt.title('Амплитуда светового поля БПФ, согласно варианту')
-plt.plot(u, np.abs(F_analytical), label = 'Аналитическое', c = 'k', lw = 3)
-plt.title('Амплитуда светового поля БПФ и аналитическое, согласно варианту')
-plt.legend()
-plt.grid()
-
-plt.subplot(2, 2, 4)
-plt.plot(u, np.angle(F), label='БПФ', c = 'c', lw = 10)
-plt.title('Фаза светового поля БПФ, согласно варианту')
-plt.plot(u, np.angle(F_analytical), label='Аналитическое', c = 'k', lw = 3)
-plt.title('Фаза светового поля БПФ и аналитическое, согласно варианту')
-plt.legend()
-plt.grid()
-
-# двухмерная функция, согласно варианту
-light_2d = lambda x, y: np.sin(4*np.pi*x) * np.sin(4*np.pi*y)
-X, Y = np.meshgrid(x, x)
-f_2d = light_2d(X, Y)
-
-#графики исходного сигнала в 2D, согласно варианту
-fig, arr = plt.subplots(1, 2, figsize=(10, 5))
-amp = arr[0].imshow(np.abs(f_2d), cmap='hot', interpolation='nearest')
-arr[0].set_title('Амплитуда светового поля в 2D, согласно варианту')
-fig.colorbar(amp, ax=arr[0])
-phase = arr[1].imshow(np.angle(f_2d), cmap='hot', interpolation='nearest')
-arr[1].set_title('Фаза светового поля в 2D, согласно варианту')
-fig.colorbar(phase, ax=arr[1])
-plt.show()
-
-# двухмерное быстрое преобразование Фурье
-def bpf_2d(Z, a, b, N, M):
-    for i in range(N):
-      h = (b - a) / (N - 1)
-      Z[:, i] = bpf(Z[:, i], N, M, h)
-    for i in range(N):
-      h = (b - a) / (N - 1)
-      Z[i, :] = bpf(Z[i, :], N, M, h)
-    return Z
-
-F_2d = f_2d.astype(np.complex128) # в переменную записываем двухмерную функцию, массив комплексных чисел
-F_2d = bpf_2d(F_2d, a1, a2, N, M) # двухмерное БПФ
-fig, arr = plt.subplots(1, 2, figsize=(10, 5))
-amp = arr[0].imshow(np.abs(F_2d), cmap='hot', interpolation='nearest')
-arr[0].set_title('Амплитуда светового поля БПФ в 2D, согласно варианту')
-fig.colorbar(amp, ax=arr[0])
-phase = arr[1].imshow(np.angle(F_2d), cmap='hot', interpolation='nearest')
-arr[1].set_title('Фаза светового поля БПФ в 2D, согласно варианту')
-fig.colorbar(phase, ax=arr[1])
-plt.show()
-
-analytical_2D = np.zeros((N, N), dtype=complex) # создание комплексного массива для аналитического двухмерного решения
-
-# Двухмерное аналитическое решение
-for i in range(N):
-  for j in range(N):
-    analytical_2D[i][j] = F_analytical[i] * F_analytical[j]
-
-#графики исходного сигнала аналитически в 2D, согласно варианту
-fig, arr = plt.subplots(1, 2, figsize=(15, 5))
-arr[0].imshow(np.abs(analytical_2D), cmap='hot', interpolation='nearest')
-arr[0].set_title('Амплитуда светового поля аналитическое в 2D, согласно варианту')
-fig.colorbar(amp, ax=arr[0])
-phase = arr[1].imshow(np.angle(analytical_2D), cmap='hot', interpolation='nearest')
-arr[1].set_title('Фаза светового поля аналитическое в 2D, согласно варианту')
-fig.colorbar(phase, ax=arr[1])
-plt.show()
-
-#GAUSS
-gauss = lambda x: np.exp(-x ** 2)
-f_gauss = gauss(x)
-F_gauss = bpf(f_gauss, N, M, hx) # результат после БПФ
-
-#графики гауссова пучка
-plt.figure(figsize=(20,10))
-
-plt.subplot(2, 2, 1)
-plt.plot(x, np.abs(f_gauss))
-plt.title('Амплитуда гауссова пучка')
-plt.grid()
-
-plt.subplot(2, 2, 2)
-plt.plot(x, np.angle(f_gauss))
-plt.title('Фаза гауссова пучка')
-plt.grid()
-
-plt.subplot(2, 2, 3)
-plt.plot(u, np.abs(F_gauss))
-plt.title('Амплитуда гауссова пучка БПФ')
-plt.grid()
-
-plt.subplot(2, 2, 4)
-plt.plot(u, np.angle(F_gauss))
-plt.title('Фаза гауссова пучка БПФ')
-plt.grid()
-
-interval = abs(N ** 2 / (4 * a2 * M))
-# Шаг дисретизации
-step = 2 * interval / (N - 1)
-F_rect = analitic_Furie(f_gauss) # аналитическое преобразование Фурье гауссова пучка
-
-#графики исходного сигнала БПФ
-plt.figure(figsize=(15,5))
-
-plt.subplot(1, 2, 1)
-plt.plot(u, np.abs(F_rect))
-plt.title('Амплитуда гауссова пучка аналитическое')
-plt.grid()
-
-plt.subplot(1, 2, 2)
-plt.plot(u, np.angle(F_rect))
-plt.title('Фаза гауссова пучка поля аналитически')
-plt.grid()
-
-# Двухмерный гауссов пучок
-gauss_2d = lambda x, y: np.exp(-x ** 2 - y ** 2)
-f_gauss_2d = gauss_2d(X, Y)
-
-#графики гауссова пучка в 2D
-fig, arr = plt.subplots(1, 2, figsize=(10, 5))
-amp = arr[0].imshow(np.abs(f_gauss_2d), cmap='hot', interpolation='nearest')
-arr[0].set_title('Амплитуда гауссова пучка в 2d')
-phase = arr[1].imshow(np.angle(f_gauss_2d), cmap='hot', interpolation='nearest')
-arr[1].set_title('Фаза гауссова пучка в 2d')
-fig.colorbar(phase, ax=arr[1])
-plt.show()
-
-# Двухмерное БПФ двухмерного гауссова пучка
-F_gauss_2d = f_gauss_2d.astype(np.complex128)
-F_gauss_2d = bpf_2d(F_gauss_2d, a1, a2, N, M)
-
-#графики гауссова пучка БПФ в 2D
-fig, arr = plt.subplots(1, 2, figsize=(10, 5))
-amp = arr[0].imshow(np.abs(F_gauss_2d), cmap='hot', interpolation='nearest')
-arr[0].set_title('Амплитуда гауссова пучка БПФ в 2d')
-phase = arr[1].imshow(np.angle(F_gauss_2d), cmap='hot', interpolation='nearest')
-arr[1].set_title('Фаза гауссова пучка БПФ в 2d')
-fig.colorbar(phase, ax=arr[1])
-plt.show()
-
-# Аналитическое двухмерное преобразование Фурье для гауссова пучка
-F_gauss_2d_analit = np.zeros((N, N), dtype=complex)
-for i in range(N):
-  for j in range(N):
-    F_gauss_2d_analit[i][j] = F_rect[i] * F_rect[j]
-
-#графики   аналитически гауссова пучка БПФ в 2D
-fig, arr = plt.subplots(1, 2, figsize=(10, 5))
-amp = arr[0].imshow(np.abs(F_gauss_2d_analit), cmap='hot', interpolation='nearest')
-arr[0].set_title('Амплитуда гауссова пучка аналитически в 2d')
-phase = arr[1].imshow(np.angle(F_gauss_2d_analit), cmap='hot', interpolation='nearest')
-arr[1].set_title('Фаза гауссова пучка аналитически в 2d')
-fig.colorbar(phase, ax=arr[1])
-plt.show()
+    #Выходной сигнал
+    hx = (b - a) / m
+    hxi = (q - p) / 1000
+    x = lambda k: a + k * hx
+    xi = lambda l: p + l * hxi
+    F = lambda l: (hx * sum([jv(2, x(k)*xi(l)*alpha) * x(k) * func(x(k))
+                             for k in range(0, n)]))
+    Fl = [F(l) for l in range(0, n+1)]
+    draw("График амплитуды выходного сигнала", np.arange(p, q, (q - p) / 1001), np.abs(Fl))
+    draw("График фазы выходного сигнала", np.arange(p, q, (q - p) / 1001), np.angle(Fl))
--- lab2/lab2.py
+++ lab2/lab2.py
@@ -1,375 +1,136 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# -------------------------- Исходные параметры --------------------------
-a1 = -5.0
-a2 = 5.0
-a = a2  # Половина интервала
-N = 1 << 10  # Количество точек по x
-M = 1 << 16  # Количество точек для нулевого дополнения при БПФ
-x = np.linspace(a1, a2, N, endpoint=False)
-hx = (a2 - a1) / N
+# Параметры
+a1 = -5
+a2 = 5
+N = 1 << 10
 
-# Частотная область
-# Определение частотного шага и диапазона
+# Функция, согласно 3 варианту
+light = lambda x: np.exp(2 * np.pi * 1j * x) + np.exp(-5 * np.pi * 1j * x)
+x = np.linspace(a1, a2, N, endpoint=False)
+f = light(x)
+
+M = 1 << 16
 b2 = (N * N) / (4 * a2 * M)
 b1 = -b2
-u = np.linspace(b1, b2, N, endpoint=False)  # Массив частот
+u = np.linspace(b1, b2, N, endpoint=False)
+hx = (a2 - a1) / N
 
-
-# -------------------------- Функция --------------------------
-# f(x) = e^{2 π i x} + e^{-5 π i x}
-def f_variant(x):
-    return np.exp(2j * np.pi * x) + np.exp(-5j * np.pi * x)
-
-f = f_variant(x)
-
-def f_analytic(u, a=5):
-    # Вычисляем F(u) аналитически
-    # Аккуратно обрабатываем особые точки
-    def val(u):
-        part1_num = np.sin(2 * np.pi * (1 - u) * a)
-        part1_den = np.pi * (1 - u)
-        part2_num = np.sin(2 * np.pi * (-5 - u) * a)
-        part2_den = np.pi * (-5 - u)
-
-        # Проверка особых точек
-        # u близко к 1?
-        if np.isclose(u, 1.0, atol=1e-14):
-            part1 = 2 * a  # Предел при u->1
-        else:
-            part1 = part1_num / part1_den
-
-        # u близко к -5?
-        if np.isclose(u, -5.0, atol=1e-14):
-            part2 = 2 * a  # Предел при u->-5
-        else:
-            part2 = part2_num / part2_den
-
-        return part1 + part2
-
-    # Применяем поэлементно
-    return np.array([val(ui) for ui in u], dtype=complex)
-
-
-# -------------------------- Численное преобразование Фурье методом прямоугольников --------------------------
-# Используем прямое определение интеграла:
-# F(u) ≈ Σ f(x_j) * e^{-i2πx_j u} * hx, j=0..N-1
-# Здесь для каждого u из массива u вычисляем интеграл.
-
-def numeric_fourier_transform_rect(f_vals, x_vals, u_vals):
-    F_vals = np.zeros_like(u_vals, dtype=complex)
-    for i_u, uu in enumerate(u_vals):
-        # e^{-i2πx_j u}
-        integrand = f_vals * np.exp(-2j * np.pi * x_vals * uu)
-        F_vals[i_u] = np.sum(integrand) * hx
-    return F_vals
-
-
-# -------------------------- Быстрое преобразование Фурье (БПФ) --------------------------
-# Реализация с нулевым дополнением и учетом hx.
-def bpf(f_vals, N, M, hx):
+# БПФ
+def bpf(f, N, M, hx):
     k = (M - N) // 2
-    F = np.insert(f_vals, 0, np.zeros(k))
+    F = np.insert(f, 0, np.zeros(k))
     F = np.append(F, np.zeros(k))
+
     F = np.fft.fftshift(F)
     F = np.fft.fft(F) * hx
     F = np.fft.fftshift(F)
+
     F = F[len(F) // 2 - N // 2: len(F) // 2 + N // 2]
     return F
 
-# -------------------------- Гауссов пучок для проверки реализации --------------------------
-gauss = lambda x: np.exp(-x ** 2)
-f_gauss = gauss(x)
+F = bpf(f, N, M, hx)
 
-F_gauss_fft = bpf(f_gauss, N, M, hx)  # БПФ для гаусса
+# Аналитическое преобразование Фурье для 3 варианта
+analytical_F = lambda u: (np.sinc(u - 1) + np.sinc(u + 5)) * 2 * np.pi
+F_analytical = analytical_F(u)
 
-# Аналитическое преобразование гаусса известное:
-# Преобразование Фурье от e^{-x^2} с нормировкой 2π даёт также гауссову форму.
-# Для конечного отрезка приближение будет близко к гауссу. Для сравнения просто используем численный интеграл:
-F_gauss_rect = numeric_fourier_transform_rect(f_gauss, x, u)
+# Стандартное численное интегрирование
+def numerical_integration(f, x, u):
+    h_x = x[1] - x[0]
+    result = np.zeros_like(u, dtype=complex)
+    for i, ui in enumerate(u):
+        result[i] = np.sum(f * np.exp(-2j * np.pi * ui * x)) * h_x
+    return result
 
+F_numerical = numerical_integration(f, x, u)
+
+# Графики исходного сигнала, согласно варианту
 plt.figure(figsize=(20, 10))
-plt.subplot(2, 2, 1)
-plt.plot(x, np.abs(f_gauss))
-plt.title('Амплитуда гауссова пучка')
-plt.grid()
 
-plt.subplot(2, 2, 2)
-plt.plot(x, np.angle(f_gauss))
-plt.title('Фаза гауссова пучка')
-plt.grid()
-
-plt.subplot(2, 2, 3)
-plt.plot(u, np.abs(F_gauss_fft), label='БПФ', c='c', lw=5)
-plt.plot(u, np.abs(F_gauss_rect), label='Прямоугольники', c='r', lw=2)
-plt.title('Амплитуда гауссова пучка после преобразования')
-plt.legend()
-plt.grid()
-
-plt.subplot(2, 2, 4)
-plt.plot(u, np.angle(F_gauss_fft), label='БПФ', c='c', lw=5)
-plt.plot(u, np.angle(F_gauss_rect), label='Прямоугольники', c='r', lw=2)
-plt.title('Фаза гауссова пучка после преобразования')
-plt.legend()
-plt.grid()
-
-plt.show()
-
-plt.figure(figsize=(20, 10))
-plt.subplot(2, 2, 1)
-plt.plot(x, np.abs(f_gauss))
-plt.title('Амплитуда гауссова пучка')
-plt.grid()
-
-plt.subplot(2, 2, 2)
-plt.plot(x, np.angle(f_gauss))
-plt.title('Фаза гауссова пучка')
-plt.grid()
-
-plt.subplot(2, 2, 3)
-plt.plot(u, np.abs(F_gauss_fft), label='БПФ', c='c', lw=5)
-plt.plot(u, np.abs(F_gauss_rect), label='Прямоугольники', c='r', lw=2)
-plt.title('Амплитуда гауссова пучка после преобразования')
-plt.legend()
-plt.grid()
-
-plt.subplot(2, 2, 4)
-plt.plot(u, np.angle(F_gauss_fft), label='БПФ', c='c', lw=5)
-plt.plot(u, np.angle(F_gauss_rect), label='Прямоугольники', c='r', lw=2)
-plt.title('Фаза гауссова пучка после преобразования')
-plt.legend()
-plt.grid()
-
-plt.show()
-
-plt.figure(figsize=(20, 10))
-plt.subplot(2, 2, 1)
-plt.plot(x, np.abs(f_gauss))
-plt.title('Амплитуда гауссова пучка')
-plt.grid()
-
-plt.subplot(2, 2, 2)
-plt.plot(x, np.angle(f_gauss))
-plt.title('Фаза гауссова пучка')
-plt.grid()
-
-plt.subplot(2, 2, 3)
-plt.plot(u, np.abs(F_gauss_fft), label='БПФ', c='c', lw=5)
-plt.plot(u, np.abs(F_gauss_rect), label='Прямоугольники', c='r', lw=2)
-plt.title('Амплитуда гауссова пучка после преобразования')
-plt.legend()
-plt.grid()
-
-plt.subplot(2, 2, 4)
-plt.plot(u, np.angle(F_gauss_fft), label='БПФ', c='c', lw=5)
-plt.plot(u, np.angle(F_gauss_rect), label='Прямоугольники', c='r', lw=2)
-plt.title('Фаза гауссова пучка после преобразования')
-plt.legend()
-plt.grid()
-
-plt.show()
-# -------------------------- Вычисления--------------------------
-
-F_fft = bpf(f, N, M, hx)  # Численный метод (БПФ)
-F_an = f_analytic(u)  # Аналитический результат
-
-# -------------------------- Построение графиков--------------------------
-plt.figure(figsize=(20, 10))
 plt.subplot(2, 2, 1)
 plt.plot(x, np.abs(f))
-plt.title('Амплитуда исходного сигнала')
+plt.title('Амплитуда светового поля, согласно варианту')
 plt.grid()
 
 plt.subplot(2, 2, 2)
 plt.plot(x, np.angle(f))
-plt.title('Фаза исходного сигнала')
+plt.title('Фаза светового поля, согласно варианту')
 plt.grid()
 
 plt.subplot(2, 2, 3)
-plt.plot(u, np.abs(F_fft), label='БПФ', c='c', lw=5)
-plt.plot(u, np.abs(F_an), label='Аналитическое', c='k', lw=1)
-plt.title('Амплитуда результата преобразования')
+plt.plot(u, np.abs(F), label='БПФ', c='c', lw=2)
+plt.plot(u, np.abs(F_analytical), label='Аналитическое', c='k', lw=1)
+plt.plot(u, np.abs(F_numerical), label='Численное', c='r', lw=1)
+plt.title('Амплитуда светового поля')
 plt.legend()
 plt.grid()
 
 plt.subplot(2, 2, 4)
-plt.plot(u, np.angle(F_fft), label='БПФ', c='c', lw=5)
-plt.plot(u, np.angle(F_an), label='Аналитическое', c='k', lw=1)
-plt.title('Фаза результата преобразования')
+plt.plot(u, np.angle(F), label='БПФ', c='c', lw=2)
+plt.plot(u, np.angle(F_analytical), label='Аналитическое', c='k', lw=1)
+plt.plot(u, np.angle(F_numerical), label='Численное', c='r', lw=1)
+plt.title('Фаза светового поля')
 plt.legend()
 plt.grid()
 
 plt.show()
 
-# -------------------------- Двумерный случай --------------------------
-# Определим двумерную функцию для варианта:
-# f_2d(x,y) = f_variant(x)*f_variant(y), но в варианте требуется именно отличное поле.
-# Можно использовать заданное f_variant(x) для каждой координаты:
+# Двумерное преобразование Фурье
 X, Y = np.meshgrid(x, x)
-f_2d = f_variant(X) * f_variant(Y)  # двумерный аналог
+light_2d = lambda x, y: light(x) * light(y)
+f_2d = light_2d(X, Y)
 
-fig, arr = plt.subplots(1, 2, figsize=(10, 5))
-amp = arr[0].imshow(np.abs(f_2d), cmap='hot', extent=[a1, a2, a1, a2], origin='lower')
-arr[0].set_title('Амплитуда поля в 2D')
-fig.colorbar(amp, ax=arr[0])
-phase = arr[1].imshow(np.angle(f_2d), cmap='hot', extent=[a1, a2, a1, a2], origin='lower')
-arr[1].set_title('Фаза поля в 2D')
-fig.colorbar(phase, ax=arr[1])
-plt.show()
-
-
-# Двумерное БПФ: сначала по строкам, потом по столбцам
-def bpf_2d(Z, a, b, N, M):
-    # Применяем бпф по столбцам
-    for i in range(N):
-        h = (b - a) / N
-        Z[:, i] = bpf(Z[:, i], N, M, h)
-    # Применяем бпф по строкам
-    for i in range(N):
-        h = (b - a) / N
-        Z[i, :] = bpf(Z[i, :], N, M, h)
+# Двумерное БПФ
+def bpf_2d(Z, N, M, hx):
+    for i in range(Z.shape[0]):
+        Z[i, :] = bpf(Z[i, :], N, M, hx)
+    for j in range(Z.shape[1]):
+        Z[:, j] = bpf(Z[:, j], N, M, hx)
     return Z
 
+F_2d = bpf_2d(f_2d.astype(np.complex128), N, M, hx)
 
-F_2d = f_2d.astype(np.complex128)
-F_2d = bpf_2d(F_2d, a1, a2, N, M)
+# Аналитическое двумерное преобразование
+F_2d_analytical = np.outer(F_analytical, F_analytical)
 
-fig, arr = plt.subplots(1, 2, figsize=(10, 5))
-amp = arr[0].imshow(np.abs(F_2d), cmap='hot', extent=[b1, b2, b1, b2], origin='lower')
-arr[0].set_title('Амплитуда поля после БПФ в 2D')
-fig.colorbar(amp, ax=arr[0])
-phase = arr[1].imshow(np.angle(F_2d), cmap='hot', extent=[b1, b2, b1, b2], origin='lower')
-arr[1].set_title('Фаза поля после БПФ в 2D')
-fig.colorbar(phase, ax=arr[1])
+# Графики двумерного преобразования
+fig, axes = plt.subplots(2, 2, figsize=(15, 10))
+
+axes[0, 0].imshow(np.abs(f_2d), extent=[a1, a2, a1, a2], cmap='hot')
+axes[0, 0].set_title('Амплитуда исходного поля')
+axes[0, 1].imshow(np.angle(f_2d), extent=[a1, a2, a1, a2], cmap='hot')
+axes[0, 1].set_title('Фаза исходного поля')
+
+axes[1, 0].imshow(np.abs(F_2d), extent=[b1, b2, b1, b2], cmap='hot')
+axes[1, 0].set_title('Амплитуда БПФ')
+axes[1, 1].imshow(np.angle(F_2d), extent=[b1, b2, b1, b2], cmap='hot')
+axes[1, 1].set_title('Фаза БПФ')
+
+plt.tight_layout()
 plt.show()
 
-# Аналитическое двухмерное решение можно получить как произведение двух одномерных решений:
-F_an_2d = np.outer(F_an, F_an)
-
-fig, arr = plt.subplots(1, 2, figsize=(10, 5))
-amp = arr[0].imshow(np.abs(F_an_2d), cmap='hot', extent=[b1, b2, b1, b2], origin='lower')
-arr[0].set_title('Амплитуда аналитически в 2D')
-fig.colorbar(amp, ax=arr[0])
-phase = arr[1].imshow(np.angle(F_an_2d), cmap='hot', extent=[b1, b2, b1, b2], origin='lower')
-arr[1].set_title('Фаза аналитически в 2D')
-fig.colorbar(phase, ax=arr[1])
-plt.show()
-
-# Аналогично можно выполнить для двумерного гаусса:
-gauss_2d = lambda x, y: np.exp(-x ** 2 - y ** 2)
-f_gauss_2d = gauss_2d(X, Y)
-
-fig, arr = plt.subplots(1, 2, figsize=(10, 5))
-amp = arr[0].imshow(np.abs(f_gauss_2d), cmap='hot', extent=[a1, a2, a1, a2], origin='lower')
-arr[0].set_title('Амплитуда гаусса в 2D')
-phase = arr[1].imshow(np.angle(f_gauss_2d), cmap='hot', extent=[a1, a2, a1, a2], origin='lower')
-arr[1].set_title('Фаза гаусса в 2D')
-fig.colorbar(phase, ax=arr[1])
-plt.show()
-
-F_gauss_2d = f_gauss_2d.astype(np.complex128)
-F_gauss_2d = bpf_2d(F_gauss_2d, a1, a2, N, M)
-
-fig, arr = plt.subplots(1, 2, figsize=(10, 5))
-amp = arr[0].imshow(np.abs(F_gauss_2d), cmap='hot', extent=[b1, b2, b1, b2], origin='lower')
-arr[0].set_title('Амплитуда гаусса после БПФ в 2D')
-phase = arr[1].imshow(np.angle(F_gauss_2d), cmap='hot', extent=[b1, b2, b1, b2], origin='lower')
-arr[1].set_title('Фаза гаусса после БПФ в 2D')
-fig.colorbar(phase, ax=arr[1])
-plt.show()
-
-# Аналитический 2D для гаусса - произведение одномерных результатов:
-# Для гаусса аналитически: Фурье также гаусс. Для упрощения возьмём результат численно рассчитанный одномерно:
-F_gauss_1d = numeric_fourier_transform_rect(f_gauss, x, u)
-F_gauss_2d_an = np.outer(F_gauss_1d, F_gauss_1d)
-
-fig, arr = plt.subplots(1, 2, figsize=(10, 5))
-amp = arr[0].imshow(np.abs(F_gauss_2d_an), cmap='hot', extent=[b1, b2, b1, b2], origin='lower')
-arr[0].set_title('Амплитуда гаусса аналитически в 2D')
-phase = arr[1].imshow(np.angle(F_gauss_2d_an), cmap='hot', extent=[b1, b2, b1, b2], origin='lower')
-arr[1].set_title('Фаза гаусса аналитически в 2D')
-fig.colorbar(phase, ax=arr[1])
-plt.show()
-
-# ---------------------------------------------------
-# Исследуем влияние N и M.
-
-# Набор значений N для исследования (все степени двойки)
+# Исследование влияния параметров N и M
 N_values = [256, 512, 1024, 2048]
-fixed_N = 1024
+M_values = [N, N * 2, N * 4]
 
-# Функция, которая возвращает координатную сетку и частотную сетку в зависимости от N, M
-def get_grids(N, M, a1=-5, a2=5):
-    x = np.linspace(a1, a2, N, endpoint=False)
+for N in N_values:
     hx = (a2 - a1) / N
-    b2 = (N * N) / (4 * a2 * M)
-    b1 = -b2
-    u = np.linspace(b1, b2, N, endpoint=False)
-    return x, u, hx
+    x = np.linspace(a1, a2, N, endpoint=False)
+    f = light(x)
 
-# Исследуем влияние увеличения/уменьшения N при M = N
-plt.figure(figsize=(20, 10))
+    for M in M_values:
+        b2 = (N * N) / (4 * a2 * M)
+        b1 = -b2
+        u = np.linspace(b1, b2, N, endpoint=False)
 
-for i, NN in enumerate(N_values, 1):
-    MM = NN  # M = N
-    x, u, hx = get_grids(NN, MM, a1, a2)
-    f_vals = f_variant(x)
-    F_bpf = bpf(f_vals, NN, MM, hx)
-    F_an = f_analytic(u)
+        F = bpf(f, N, M, hx)
 
-    plt.subplot(2, 2, i)
-    plt.plot(u, np.abs(F_bpf), label=f'БПФ N={NN}, M={MM}', lw=2)
-    plt.plot(u, np.abs(F_an), label='Аналитическое', lw=1)
-    plt.title(f'Амплитуда при N={NN}, M=N')
-    plt.legend()
-    plt.grid()
+        plt.figure(figsize=(10, 5))
+        plt.plot(u, np.abs(F), label=f'N={N}, M={M}')
+        plt.title('Амплитуда светового поля для разных N и M')
+        plt.legend()
+        plt.grid()
+        plt.show()
 
-plt.suptitle('Влияние изменения N при M=N на амплитуду Фурье-образа')
-plt.show()
-
-N_fixed = fixed_N
-M_new = 2 * N_fixed
-x_fixed, u_fixed, hx_fixed = get_grids(N_fixed, N_fixed, a1, a2)
-x_newM, u_newM, hx_newM = get_grids(N_fixed, M_new, a1, a2)
-
-f_fixed = f_variant(x_fixed)
-f_newM = f_variant(x_newM) # хотя сетка x такая же, просто u_newM будет другой
-
-F_fixed = bpf(f_fixed, N_fixed, N_fixed, hx_fixed)
-F_newM = bpf(f_newM, N_fixed, M_new, hx_newM)
-F_an_fixed = f_analytic(u_fixed)
-F_an_newM = f_analytic(u_newM)
-
-plt.figure(figsize=(20, 10))
-
-plt.subplot(2, 2, 1)
-plt.plot(u_fixed, np.abs(F_fixed), label='БПФ при M=N', lw=2)
-plt.plot(u_fixed, np.abs(F_an_fixed), label='Аналитическое', lw=1)
-plt.title('Амплитуда при M = N')
-plt.legend()
-plt.grid()
-
-plt.subplot(2, 2, 2)
-plt.plot(u_newM, np.abs(F_newM), label='БПФ при M=2N', lw=2)
-plt.plot(u_newM, np.abs(F_an_newM), label='Аналитическое', lw=1)
-plt.title('Амплитуда при M = 2N')
-plt.legend()
-plt.grid()
-
-plt.subplot(2, 2, 3)
-plt.plot(u_fixed, np.angle(F_fixed), label='БПФ при M=N', lw=2)
-plt.plot(u_fixed, np.angle(F_an_fixed), label='Аналитическое', lw=1)
-plt.title('Фаза при M = N')
-plt.legend()
-plt.grid()
-
-plt.subplot(2, 2, 4)
-plt.plot(u_newM, np.angle(F_newM), label='БПФ при M=2N', lw=2)
-plt.plot(u_newM, np.angle(F_an_newM), label='Аналитическое', lw=1)
-plt.title('Фаза при M = 2N')
-plt.legend()
-plt.grid()
-
-plt.suptitle('Сравнение результатов при фиксированном N и разном M')
-plt.show()
